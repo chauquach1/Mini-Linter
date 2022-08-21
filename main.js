343a932c1312b11538b110a45f56d53558239300
chauquach1 <110010009+chauquach1@users.noreply.github.com>
2022-08-20 19:05:12 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 /*
+Step 1
+
 Below, there is a string called story. We want to gather some information about the individual words
 and sentences in the string. Let’s split the string into individual words and save
 them in a new array called storyWords
 */
 
-let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
 
-let overusedWords = ['really', 'very', 'basically'];
+var story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
 
-let unnecessaryWords = ['extremely', 'literally', 'actually' ];
+var overusedWords = ['really', 'very', 'basically'];
+
+var unnecessaryWords = ['extremely', 'literally', 'actually' ];
+
+var storyWords = story.split(' ');
+
+let wordCount = storyWords.length;
+
 
 /*
  Step 2: Log how many words there are in this story to the console.
 */
 
-
+console.log (wordCount);
 
 
 /* Step 3
@@ -23,8 +31,13 @@ array to filter out these words. Save the
 remaining words in an array called betterWords. 
 There are several ways that you could achieve this.
 */
+var betterWords = [];
 
-
+storyWords.forEach(function (word){
+    if (!unnecessaryWords.includes(word)) {
+        betterWords.push(word);
+    }
+});
 
 /*
 Step 4
@@ -35,7 +48,27 @@ these overused words. There are two ways to achieve this.
 Try it on your own first.
 */
 
+let rCount = 0;
+let vCount = 0;
+let bCount = 0;
 
+storyWords.forEach(function (word){
+    if (word.includes('really')) {
+        rCount++;
+    }; 
+
+    if (word.includes('very')) {
+        vCount++;
+    }; 
+    
+    if (word.includes('basically')) {
+        bCount++;
+    };
+});
+
+console.log(rCount);
+console.log(vCount);
+console.log(bCount);
 
 /*
 Step 5
@@ -48,6 +81,15 @@ variable for each word that has a period or exclamation mark as
 its final character.
 */
 
+let sentences = 0
+
+storyWords.forEach(function (word){
+    if (word.includes('.') || word.includes('!')){
+        sentences++;
+    }
+})
+
+console.log(sentences);
 
 /*
 Step 6
@@ -62,11 +104,14 @@ a challenge, create a function that logs all of them
 with some formatting.
 */
 
+console.log(`Your word count is ${wordCount}. Your sentence count is ${sentences}. You used "really" ${rCount} times. You used "very" ${vCount} times. You used "basically" ${bCount} time.`)
+
 /*
 Step 7
 Now, log the betterWords array to the console as a single string.
 */
 
+console.log(betterWords.join(' '));
 
 /*
 Step 8
